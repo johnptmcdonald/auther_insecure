95a83ed28d97af9bec8335587abf15f4d3e83d10
Thomas Kelly <tmkelly28@gmail.com>
2018-04-02 15:53:37 -0400

    refactor

--- browser/store.js
+++ browser/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
-import loggerMiddleware from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux'
+import loggerMiddleware from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-import rootReducer from './redux';
+import rootReducer from './redux'
 
 export default createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware))
-);
+)
