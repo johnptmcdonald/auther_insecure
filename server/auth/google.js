e1d07bcfcc910563394a96927cb7863e0ba6ffdb
Thomas Kelly <tmkelly28@gmail.com>
2018-04-03 15:10:55 -0400

    saving

--- server/auth/google.js
+++ server/auth/google.js
@@ -9,7 +9,7 @@ passport.use(
   new GoogleStrategy({
     clientID: '238524570915-ivf9lnhm9bsfq13cle5ap8s28d4lmhrp.apps.googleusercontent.com',
     clientSecret: 'GST6VQnVmhx1YIB1vDXXB3PF',
-    callbackURL: '/api/auth/google/verify'
+    callbackURL: '/auth/google/verify'
   },
   (token, refreshToken, profile, done) => {
     const info = {
